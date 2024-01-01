f59316a0d7b7335fad862aed5e1d942964023962
Al Rodriguez <ProgrammerAl@Outlook.com>
2024-01-01 17:31:21 -0500

    Finalized script for packaging up apps for deployment

--- feedback-system/src/package-for-deployment.ps1
+++ feedback-system/src/package-for-deployment.ps1
@@ -8,7 +8,9 @@ New-Item -Path $publishedAppsDirectory -ItemType Directory
 
 #Sanity check, make sure we're running from the proper loction
 Set-Location "$PSScriptRoot/FeedbackFunctionsApp"
-
 & "dotnet" publish -c Release
-
 Compress-Archive -Path "$PSScriptRoot/FeedbackFunctionsApp/bin/Release/net8.0/publish/*" -DestinationPath "$publishedAppsDirectory/functions.zip"
+
+Set-Location "$PSScriptRoot/FeedbackWebApp"
+& "dotnet" publish -c Release
+Compress-Archive -Path "$PSScriptRoot/FeedbackWebApp/bin/Release/net8.0/*" -DestinationPath "$publishedAppsDirectory/webapp.zip"
