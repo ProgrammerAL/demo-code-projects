6340df0ea34ab75e05dd27e474c826fdd6d67291
Al Rodriguez <ProgrammerAl@Outlook.com>
2024-01-01 15:44:35 -0500

    Got the web app pretty much done

--- feedback-system/src/FeedbackWebApp/Program.cs
+++ feedback-system/src/FeedbackWebApp/Program.cs
@@ -1,4 +1,6 @@
 using FeedbackWebApp;
+using FeedbackWebApp.Configs;
+using FeedbackWebApp.HttpClients;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,6 +9,29 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureConfig<ApiConfig>(builder);
+
+builder.Services.AddHttpClient<IFeedbackHttpClient, FeedbackHttpClient>((serviceProvider, client) =>
+{
+    var apiConfig = serviceProvider.GetRequiredService<ApiConfig>();
+
+    var baseEndpoint = apiConfig.FeedbackEndpoint;
+    if (!baseEndpoint.EndsWith('/'))
+    {
+        baseEndpoint += "/";
+    }
+
+    client.BaseAddress = new Uri(baseEndpoint);
+});
 
 await builder.Build().RunAsync();
+
+
+static T ConfigureConfig<T>(WebAssemblyHostBuilder builder)
+    where T : class, new()
+{
+    var config = new T();
+    builder.Configuration.Bind(typeof(T).Name, config);
+    _ = builder.Services.AddSingleton(config);
+    return config;
+}
