4b7bb2988075430ab2ddbb5af77ca66de6f81165
Al Rodriguez <ProgrammerAl@Outlook.com>
2024-01-01 16:05:22 -0500

    Started work on a script to package up the apps

---
+++ feedback-system/src/package-for-deployment.ps1
@@ -0,0 +1,14 @@
+$publishedAppsDirectory = "$PSScriptRoot/published-apps"
+
+if (Test-Path -Path $publishedAppsDirectory) {
+    Remove-Item -Path $publishedAppsDirectory -Recurse
+}
+
+New-Item -Path $publishedAppsDirectory -ItemType Directory
+
+#Sanity check, make sure we're running from the proper loction
+Set-Location "$PSScriptRoot/FeedbackFunctionsApp"
+
+& "dotnet" publish -c Release
+
+Compress-Archive -Path "$PSScriptRoot/FeedbackFunctionsApp/bin/Release/net8.0/publish/*" -DestinationPath "$publishedAppsDirectory/functions.zip"
